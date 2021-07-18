61788092cbd168f03394c0f6039cca5b09126a58
FelipeDeBoa <53651862+felipelisboadoc@users.noreply.github.com>
2021-07-18 11:42:46 -0300

    Form cartao

---
+++ js/eventos.js
@@ -0,0 +1,40 @@
+document.getElementById('numero').addEventListener('keyup', function () {
+    let numero = document.getElementById('numero').value;
+    document.getElementById('card_number').innerHTML = numero;
+});
+
+document.getElementById('nome').addEventListener('keyup', function () {
+    let nome = document.getElementById('nome').value;
+    document.getElementById('card_name').innerHTML = nome;
+});
+
+//document.getElementById('mes').addEventListener('change', function () {
+//    let mes = document.getElementById('mes').value;
+//   let ano = document.getElementById('ano').value;
+//    document.getElementById('card_date').innerHTML = mes+"/"+ano;    
+//});
+
+//document.getElementById('ano').addEventListener('change', function () {
+//    let mes = document.getElementById('mes').value;
+//    let ano = document.getElementById('ano').value;
+//    document.getElementById('card_date').innerHTML = mes+"/"+ano;    
+//});
+// 
+
+document.querySelectorAll('[data-action="date"]').forEach(function(element){
+    element.addEventListener('change', function () {
+    let mes = document.getElementById('mes').value;
+    let ano = document.getElementById('ano').value;
+    document.getElementById('card_date').innerHTML = mes+"/"+ano;    
+  });
+});
+
+document.getElementById('codigo').addEventListener('focus', function () {
+    document.getElementById('lado-esquerdo').style.transform = 'rotateY(180deg)';
+    
+});
+
+document.getElementById('codigo').addEventListener('blur', function () {
+    document.getElementById('lado-esquerdo').style.transform = 'rotateY(0deg)';
+    
+});
